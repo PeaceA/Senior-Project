0adbbc75ca7e63734fcc0c246d4f34e4bd4498f4
Mahia Tasneem <mahiatasneem@Mahias-MacBook-Pro.local>
2020-03-03 23:23:16 -0500

    Modified data page to add more fields.

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,5 +1,19 @@
 class User {
   String firstName;
   String lastName;
+  String id;
+  String email;
+  String phoneNumber;
   String role;
+}
+
+class Student extends User {
+  String classification;
+  String startSemester;
+  String major;
+}
+
+class Advisor extends User {
+  String officeBuilding;
+  String roomNumber;
 }
