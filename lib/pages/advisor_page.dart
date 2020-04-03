217d6ec142076ca35ac310de1f9552eb012c36c8
Nelly <shaunella_thomas@yahoo.com>
2020-04-03 12:15:44 -0400

    Advisor Page starter

--- lib/pages/advisor_page.dart
+++ lib/pages/advisor_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class AdvisorPage extends StatefulWidget {
+// class AdvisorPage extends StatefulWidget {
 
-}
+// }
