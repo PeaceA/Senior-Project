7453296c62d00e19688c76c8a8f423fbaa75e250
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2020-05-05 17:51:44 -0400

    Registration frames

---
+++ lib/frames/advisor_registration.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AdvisorRegistration extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text("Registration"),
+        ),
+    body: Center(
+      child: Text('You have pressed the button times.')
+    ),
+    backgroundColor: Colors.black,
+  );
+  }
+}
